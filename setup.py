18b1c4d716429ba150eee0fc293f022cef77a8ac
idc9 <idc9@cornell.edu>
2020-09-17 15:02:44 -0700

    fix comma

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ install_requires = ['numpy',
                     'networkx',
                     'statsmodels',
                     'cvxpy'
-                    'textwrap'
                     ]
 
 
